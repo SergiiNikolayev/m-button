3af9cf50eb7fc17a542871286f745b5335c6385a
Serge Nikolayev <sergey.nikolaev@litslink.com>
2023-03-14 15:37:31 +0200

    add animation

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,7 @@
 import React, { ReactNode, useState } from 'react';
 import styles from './index.module.css';
+import { motion } from "framer-motion";
+import { LongPressDetectEvents, useLongPress } from 'use-long-press';
 
 export enum ISizeList {
   small = 'small',
@@ -13,34 +15,77 @@ interface IButtonProps {
   closeIcon?: ReactNode
   onClick?: () => void
   style?: object
+  animate?: object
 }
+
 const sizeList = {
-  small: '50px',
-  big: '75px',
+  small: '40px',
+  big: '70px',
 }
-function Button({ size, icon, label, closeIcon, onClick, style }: IButtonProps) {
+function Button({ size, icon, label, closeIcon, onClick, style, animate }: IButtonProps) {
   const buttonSize = sizeList[size]
   const isSmall = size === ISizeList.small
   const [open, setOpen] = useState(false);
+  const [buttonVisibility, setButtonVisibility] = useState('visible');
   const onPress = () => {
     onClick && onClick()
     return isSmall ? console.log('clicked', label) : setOpen(!open);
+    setButtonVisibility('visible')
   }
+
+  const longPressCallback = () => {
+    setButtonVisibility('hidden');
+  }
+
   const Icon = open ? !isSmall && closeIcon : icon
 
+  const callback = React.useCallback(() => {
+    console.log("Long pressed!");
+  }, []);
+
+  const longPress = useLongPress(longPressCallback, {
+    onStart: () => console.log("Press started"),
+    // onStart: onPress,
+    onFinish: () => console.log("Long press finished"),
+    // onCancel: () => console.log("Press cancelled"),
+    onCancel: () => setButtonVisibility('visible'),
+    onMove: () => console.log("Detected mouse or touch movement"),
+    threshold: 1000,
+    captureEvent: true,
+    cancelOnMovement: false,
+    detect: LongPressDetectEvents.BOTH,
+    // ...(open ? {threshold: 1000} : {}),
+    // ...(open ? {detect: LongPressDetectEvents.BOTH} : {}),
+  });
+
+  // const longPressData = open ? longPress : callback
+
+  // console.log('debug-buttonVisibility', buttonVisibility)
+
   return (
-    <div
-      onClick={onPress}
-      className={styles.container}
-      style={{
-        height: buttonSize,
-        width: buttonSize,
-        ...style,
-      }}
+    <motion.div
+      className="box"
+      // whileHover={{ scale: 1.2 }}
+      animate={animate}
+      whileTap={{ scale: 0.9 }}
+      transition={{ type: "spring", stiffness: 400, damping: isSmall ? 30 : 15 }}
     >
-      {label && isSmall && <div className={styles.label}>{label}</div>}
-      {icon && Icon}
-    </div>
+      <div
+        {...longPress()}
+        onClick={onPress} // OK
+        className={styles.container}
+        style={{
+          height: buttonSize,
+          width: buttonSize,
+          // @ts-ignore
+          // visibility: buttonVisibility,
+          ...style,
+        }}
+      >
+        {label && isSmall && <div className={styles.label}>{label}</div>}
+        {icon && Icon}
+      </div>
+    </motion.div>
   );
 }
 
--- src/components/OrigamiButton/index.tsx
+++ src/components/OrigamiButton/index.tsx
@@ -1,40 +1,43 @@
 import React, { useState } from 'react';
-import logo from '../../logo.svg';
 import styles from './index.module.css';
 import { AiOutlineClose, AiOutlineArrowDown, AiOutlineArrowUp } from 'react-icons/ai';
 import { BiCoinStack } from 'react-icons/bi';
 import { BsPiggyBank } from 'react-icons/bs';
 import { RiHandCoinLine } from 'react-icons/ri';
+import { TbLetterW } from 'react-icons/tb';
 import Button, { ISizeList } from '../Button';
+
+const iconSize = 25;
 
 const buttonConfig = [
   {
-    icon: <BiCoinStack size={35} color='white' />,
+    icon: <BiCoinStack size={iconSize} color='white' />,
     label: 'Stake',
-    margin: '-25px',
+    animate: { y: -35, x: -95},
     onClick: () => console.log('1')
   },
   {
-    icon: <AiOutlineArrowUp size={35} color='white' />,
+    icon: <AiOutlineArrowUp size={iconSize} color='white' />,
     label: 'Send',
-    margin: '-5px',
+    animate: { y: -53, x: -40},
     onClick: () => console.log('2')
   },
   {
-    icon: <AiOutlineArrowDown size={35} color='white' />,
+    icon: <AiOutlineArrowDown size={iconSize} color='white' />,
     label: 'Receive',
+    animate: { y: -60, x: 15},
     onClick: () => console.log('3')
   },
   {
-    icon: <BsPiggyBank size={35} color='white' />,
+    icon: <BsPiggyBank size={iconSize} color='white' />,
     label: 'Supply',
-    margin: '-5px',
+    animate: { y: -53, x: 70},
     onClick: () => console.log('4')
   },
   {
-    icon: <RiHandCoinLine size={33} style={{ transform: 'scaleX(-1)' }} color='white' />,
+    icon: <RiHandCoinLine size={iconSize} style={{ transform: 'scaleX(-1)' }} color='white' />,
     label: 'Borrow',
-    margin: '-25px',
+    animate: { y: -35, x: 125},
     onClick: () => console.log('5')
   },
 ]
@@ -45,22 +48,21 @@ function OrigamiButton() {
 
   return (
     <div className={styles.container}>
-      <div className={styles.smallButtonContainer}>
-        {showSmallButtons && buttonConfig.map((btn, i) =>
-          <Button
-            size={ISizeList.small}
-            label={btn.label}
-            icon={btn.icon}
-            onClick={() => console.log(i)}
-            style={{ marginBottom: btn.margin }}
-          />
-        )}
-      </div>
+      {showSmallButtons && buttonConfig.map((btn, i) =>
+        <Button
+          size={ISizeList.small}
+          label={btn.label}
+          icon={btn.icon}
+          animate={btn.animate}
+          onClick={() => console.log(i)}
+          style={{ position: 'absolute' }}
+        />
+      )}
       <Button
         size={ISizeList.big}
         label='label'
         closeIcon={<AiOutlineClose /> }
-        icon={<img src={logo} className={styles.logo} alt="logo" />}
+        icon={<TbLetterW size={40} style={{ transform: 'rotate(180deg)' }} />}
         onClick={toggleShow}
       />
     </div>
